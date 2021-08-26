200a26301b1b91413f3d24358b7bc6b0e8ebd135
Romantsov Alexander <script@vip-person.net>
2021-08-26 17:30:25 +0300

    Initial dist subtree commit

---
+++ precache-manifest.76231e094137b98fe856d0dc6b6f0499.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83db47f8a1cc6adbdcd6",
+    "url": "/fx_calc/css/app.40d1f200.css"
+  },
+  {
+    "revision": "5fbf8365db0085be6be7",
+    "url": "/fx_calc/css/chunk-vendors.8dc8ccfd.css"
+  },
+  {
+    "revision": "15478b340a8362bb79fd2a6ea0dde1a0",
+    "url": "/fx_calc/desktop.ini"
+  },
+  {
+    "revision": "83b09e00ad2513c87a63f3087306bb10",
+    "url": "/fx_calc/favicon1.ico"
+  },
+  {
+    "revision": "83b09e00ad2513c87a63f3087306bb10",
+    "url": "/fx_calc/favicons/favicon1.ico"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fx_calc/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fx_calc/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "af52e8c58a0e1768c56d6caa423b2df6",
+    "url": "/fx_calc/img/centerlogo.af52e8c5.png"
+  },
+  {
+    "revision": "dd43927ba1df7a81978d00850b4f06c7",
+    "url": "/fx_calc/index.html"
+  },
+  {
+    "revision": "83db47f8a1cc6adbdcd6",
+    "url": "/fx_calc/js/app.6efffbf3.js"
+  },
+  {
+    "revision": "5fbf8365db0085be6be7",
+    "url": "/fx_calc/js/chunk-vendors.d07ad8e1.js"
+  },
+  {
+    "revision": "5dc888fb5108ffc6f70d078b2b92a919",
+    "url": "/fx_calc/manifest.json"
+  },
+  {
+    "revision": "4f47c72c23362bc20aa210ebfa51c02e",
+    "url": "/fx_calc/normalize.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/fx_calc/robots.txt"
+  }
+]);
